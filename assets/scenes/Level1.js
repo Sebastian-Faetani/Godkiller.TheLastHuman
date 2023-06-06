f3bbdbfff0d3eae96d131c927c1dd4aa2168ee4a
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2023-06-06 05:46:00 -0300

    Nivel 1 en proceso, el personaje es invisible por alguna razon

--- assets/scenes/Level1.js
+++ assets/scenes/Level1.js
@@ -11,6 +11,27 @@ export default class Level1 extends Phaser.Scene {
 
   create() {
     
+    //Added BackGround
+    this.add.image(400, 300, "backGroundLvl1");
+
+    //adding platforms
+    let platforms = this.physics.add.staticGroup();
+    platforms.create(400, 513, "groundPlatformLvl1").refreshBody();
+    platforms.create(126, 383, "platform1Lvl1").refreshBody();
+    platforms.create(392, 295, "platform2Lvl1").refreshBody();
+    platforms.create(661, 268, "platform3Lvl1").refreshBody();
+
+    //adding player
+    this.player = this.physics.add.sprite(400, 300, "player");
+    this.player.setCollideWorldBounds(true);
+
+    //play idle animation
+    this.player.anims.play("idle", true);
+
+    //add colliders
+    this.physics.add.collider(this.player, platforms);
+
+
   }
 
   update() {
--- assets/scenes/Preloader.js
+++ assets/scenes/Preloader.js
@@ -4,15 +4,57 @@ export default class Preloader extends Phaser.Scene {
     super("Preloader");
   }
 
-  init() {
-  
-  }
-
 preload() {
   
+  this.load.image("backGroundLvl1", "./assets/images/backGroundLvl1.png");
+  this.load.image("platform1Lvl1", "./assets/images/platform1Lvl1.png");
+  this.load.image("platform2Lvl1", "./assets/images/platform2Lvl1.png");
+  this.load.image("platform3Lvl1", "./assets/images/platform3Lvl1.png");
+  this.load.image("groundPlatformLvl1", "./assets/images/groundPlatformLvl1.png");
+
+  this.load.spritesheet("player", "./assets/images/playerSpriteSheet.png", {
+    frameWidth: 39,
+    frameHeight: 65,
+  });
+
 }
 
 create() {
   
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("player", { start: 2, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "idle",
+    frames: this.anims.generateFrameNumbers("player", { start: 0, end: 1 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("player", { start: 4, end: 5 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "upLeft",
+    frames: [{ key: "player", frame: 7 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "upRight",
+    frames: [{ key: "player", frame: 6 }],
+    frameRate: 20,
+  });
+
+  this.scene.start("level1");
+
 }
 }
